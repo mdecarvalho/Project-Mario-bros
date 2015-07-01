7fc3f6ebdb2859c5cc47638fb68a85e0e7d23d71
mdecarvalho <decarvalho.michael@gmail.com>
2015-07-01 13:45:11 +0200

    margin fix

--- js/script.js
+++ js/script.js
@@ -6,27 +6,25 @@ $(document).ready(function(){
                    .attr('src','/img/Mario.gif')
                    .css({transform: 'rotateY(360deg)'})
                    .animate({left: '+=5'}, 10);
-                
-                
+               $(".character img").attr('src','/img/Mario.gif');
            }
             
             if (event.keyCode == '37') {
                $(".character")
-                   .attr('src','/img/Mario.gif')
                    .css({transform: 'rotateY(180deg)'})
                    .animate({left: '-=5'}, 10);
-                
+                $(".character img").attr('src','/img/Mario.gif');
                 
            }
         })
         $(window).keyup(function(event){
             if (event.keyCode == '39') {
-               $(".character").attr('src','/img/MarioStanding.jpg');
+               $(".character img").attr('src','/img/MarioStanding.jpg');
                
            }
             
             if (event.keyCode == '37') {
-               $(".character")
+               $(".character img")
                .attr('src','/img/MarioStanding.jpg');
                
            }
