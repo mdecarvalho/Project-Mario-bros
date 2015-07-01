151b5ecd2f7b7cf5daf14116a67d793a55f10a09
mdecarvalho <decarvalho.michael@gmail.com>
2015-07-01 13:06:12 +0200

    Add animation JS
    forward and backward

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
     
+        $(document).keydown(function(event){
+            if (event.keyCode == '39') {
+               $(".character")
+                   .attr('src','/img/Mario.gif')
+                   .css({transform: 'rotateY(360deg)'})
+                   .animate({left: '+=5'}, 10);
+                
+                
+           }
+            
+            if (event.keyCode == '37') {
+               $(".character")
+                   .attr('src','/img/Mario.gif')
+                   .css({transform: 'rotateY(180deg)'})
+                   .animate({left: '-=5'}, 10);
+                
+                
+           }
+        })
+        $(window).keyup(function(event){
+            if (event.keyCode == '39') {
+               $(".character").attr('src','/img/MarioStanding.jpg');
+               
+           }
+            
+            if (event.keyCode == '37') {
+               $(".character")
+               .attr('src','/img/MarioStanding.jpg');
+               
+           }
+        })
+        
+        
+        
+
 });
