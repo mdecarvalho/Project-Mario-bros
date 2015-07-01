02b2dd5a5a14bee77a62768eb3ebd2f540f2c69b
marie7 <marieangot@hotmail.fr>
2015-07-01 16:30:18 +0200

    background sliding

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     
         $(document).keydown(function(event){
+            
             if (event.keyCode == '39') {
                $(".character")
                    .attr('src','/img/Mario.gif')
                    .css({transform: 'rotateY(360deg)'})
                    .animate({left: '+=5'}, 10);
                $(".character img").attr('src','/img/Mario.gif');
+                $("#background").animate({left:'-=5'},1);
            }
             
             if (event.keyCode == '37') {
@@ -14,6 +16,7 @@ $(document).ready(function(){
                    .css({transform: 'rotateY(180deg)'})
                    .animate({left: '-=5'}, 10);
                 $(".character img").attr('src','/img/Mario.gif');
+                $("#background").animate({left:'+=5'},1);
                 
            }
         })
