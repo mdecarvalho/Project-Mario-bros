a5316c24b08af635123229e0233ae57583d001ef
Lodell <huynh.laurent@gmail.com>
2015-07-02 12:13:20 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
 $(document).ready(function(){
     var condition = 1; // valeur qui évite le spam sur le traitement de keydown
-    var marioPos= $('.character');
-    var charPos= marioPos.position();
-    
-    function forward(condition,position){
+
+    function forward(condition, charPos){
         if(condition == 1){
-            $(".character img").attr('src','/img/Mario.gif');
+            $(".character img").attr('src', '/img/Mario.gif');
         }
-        if(charPos.left >= 0 && charPos.left <= 150) {
+        if(charPos.left >= 0 && charPos.left <= 230) {
             $(".character")
                 .css({transform: 'rotateY(360deg)'})
-                .animate({left: '+=5'}, 5 , "linear");   
+                .animate({left: '+=5'}, 5 , "linear");
         }
         else{
             $("#background").animate({left:'-=5'},1);  
         }
-    } 
+    }
     
-    function backward(condition){
+    function backward(condition, charPos){
         if(condition == 1){
-            $(".character img").attr('src','/img/Mario.gif');
+            $(".character img").attr('src', '/img/Mario.gif');
         }
          if(charPos.left > 0 ){
              $(".character")
                 .attr('src','/img/Mario.gif')
                 .css({transform: 'rotateY(180deg)'})
-                .animate({left: '-=5'}, 5, "linear");      
+                .animate({left: '-=5'}, 5, "linear");
          }
     }
     
     function jump(condition){
         if(condition == 1){
             $("#jump_sound")[0].pause();
-            $("#jump_sound")[0].currentTime = 0; 
+            $("#jump_sound")[0].currentTime = 0;
             $(".character").animate({"top": "-=15%"}, 200, "linear");
             $(".character").animate({"top": "+=15%"}, 200, "linear");
             $("#jump_sound")[0].play();
@@ -40,31 +38,30 @@ $(document).ready(function(){
     }
     //gestion des déplacements
     $(document).keydown(function(event){
-        charPos=marioPos.position();
+        var charPos= $('.character').position();
         if (event.keyCode == '39') {
-            forward(condition,charPos);      
+            forward(condition, charPos);
         }
 
         if (event.keyCode == '37') {
-            backward(condition,charPos);
+            backward(condition, charPos);
         }
         
         if (event.keyCode == '38'){
             jump(condition);
         }
         condition = 0;
-                
+
     })
     //gestion de la fin de déplacement
     $(document).keyup(function(event){
         if (event.keyCode == '39') {
-            
-           $(".character img").attr('src','/img/MarioStanding.jpg');
+           $(".character img").attr('src', '/img/MarioStanding.jpg');
         }
 
         if (event.keyCode == '37') {
             $(".character img")
-           .attr('src','/img/MarioStanding.jpg');   
+           .attr('src', '/img/MarioStanding.jpg');
         }
         condition = 1;
     })
